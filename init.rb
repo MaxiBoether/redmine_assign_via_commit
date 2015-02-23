236236ff3939f7e5d5cb7a962aed7374db0a82d4
Lukas Pirl <git@lukas-pirl.de>
2015-02-23 18:59:51 +0100

    updated plug-in properties (init.rb)

--- init.rb
+++ init.rb
@@ -6,7 +6,8 @@ require 'redmine_assign_via_commit'
 Redmine::Plugin.register :redmine_assign_via_commit do
   name 'Assign via Commit'
   author 'Lukas Pirl'
-  description 'This plug in provides more actions that can be triggered from commit messages.'
+  description 'Set an issue\'s assignee by commit message.'
   version '0.0.1'
+  url 'https://github.com/lpirl/redmine_assign_via_commit'
   requires_redmine :version_or_higher => '2.6.0'
 end
