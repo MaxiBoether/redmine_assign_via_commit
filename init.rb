85198beccf39f337df4edc2162840e3444a86954
Lukas Pirl <git@lukas-pirl.de>
2015-03-31 19:49:06 +0200

    bump version

--- init.rb
+++ init.rb
@@ -8,7 +8,7 @@ Redmine::Plugin.register :redmine_assign_via_commit do
   author 'Lukas Pirl'
   author_url  'http://lukas-pirl.de'
   description 'Set an issue\'s assignee by commit message.'
-  version '0.0.4'
+  version '0.0.5'
   url 'https://github.com/lpirl/redmine_assign_via_commit'
   requires_redmine :version_or_higher => '2.6.0'
 end
